67d9dfcef4ef9468753a865f2a6752579d90a7e3
utkuege35 <utku1ege1@gmail.com>
2026-05-21 17:02:47 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,8 +5,8 @@
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL  = 'https://lxdtevdmdaohbuuodzio.supabase.co';   // örn: https://lxdtevdm....supabase.co
-const SUPABASE_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4ZHRldmRtZGFvaGJ1dW9kemlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyODIyMzAsImV4cCI6MjA5NDg1ODIzMH0.t_pV5zqWb2OQQdE6gFDRinOdNwyDQu2sdvUCqOH5X5g'; // sb_publishable_...
+const SUPABASE_URL  = 'BURAYA_PROJECT_URL_YAZIN';   // örn: https://lxdtevdm....supabase.co
+const SUPABASE_KEY  = 'BURAYA_PUBLISHABLE_KEY_YAZIN'; // sb_publishable_...
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -17,7 +17,7 @@ supabase.auth.onAuthStateChange((event, session) => {
     const korumalı = ['ilan-ver.html', 'profil.html', 'mesajlar.html'];
     const sayfa = window.location.pathname.split('/').pop();
     if (korumalı.includes(sayfa)) {
-      window.location.href = '/giris.html';
+      window.location.href = 'giris.html';
     }
   }
 });
