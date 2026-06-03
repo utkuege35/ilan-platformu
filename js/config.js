aad4d6ecda990d60f6e7ccee95ab26108b9793e8
utkuege35 <utku1ege1@gmail.com>
2026-06-03 10:19:15 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const SUPABASE_URL  = 'https://lxdtevdmdaohbuuodzio.supabase.co';   // örn: https://lxdtevdm....supabase.co
-const SUPABASE_KEY  = 'BURAYA_PUBLISHABLE_KEY_YAZIN'; // sb_publishable_...
+const SUPABASE_KEY  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4ZHRldmRtZGFvaGJ1dW9kemlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyODIyMzAsImV4cCI6MjA5NDg1ODIzMH0.t_pV5zqWb2OQQdE6gFDRinOdNwyDQu2sdvUCqOH5X5g'; // sb_publishable_...
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
